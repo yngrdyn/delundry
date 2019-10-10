c449938af785ca3a43a29e7214fc4ef06dcec183
Yngrid Coello <yngrdyn@gmail.com>
2019-10-10 20:47:41 +0200

    Updates

---
+++ precache-manifest.67a138a6ecd200e2d9887ce2883488e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f46720f24eb3a0867e5c5b645bffb7",
+    "url": "/delundry/index.html"
+  },
+  {
+    "revision": "0ddd1e315d1700ccd394",
+    "url": "/delundry/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "022d9c2758c24fa8d59f",
+    "url": "/delundry/static/js/2.6ad6f0a0.chunk.js"
+  },
+  {
+    "revision": "0ddd1e315d1700ccd394",
+    "url": "/delundry/static/js/main.b9245098.chunk.js"
+  },
+  {
+    "revision": "bc3e12be96d0ea291516",
+    "url": "/delundry/static/js/runtime-main.f5559bd8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/delundry/static/media/logo.25bf045c.svg"
+  }
+]);
