7b34eda5a504980963c870660641c1d0ec44c2b1
Yngrid Coello <yngrdyn@gmail.com>
2019-10-11 21:56:45 +0200

    Updates

---
+++ precache-manifest.14935ba5f8ffbd335a415cfc7439711d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba91db00dd64bda544d2c819891b2e1",
+    "url": "/delundry/index.html"
+  },
+  {
+    "revision": "12b2959269e104acd5da",
+    "url": "/delundry/static/css/main.952e0120.chunk.css"
+  },
+  {
+    "revision": "42bf3908e4ec15013adc",
+    "url": "/delundry/static/js/2.332b93b7.chunk.js"
+  },
+  {
+    "revision": "12b2959269e104acd5da",
+    "url": "/delundry/static/js/main.e6658191.chunk.js"
+  },
+  {
+    "revision": "529c5f847e989f535ea7",
+    "url": "/delundry/static/js/runtime-main.95f565fc.js"
+  },
+  {
+    "revision": "e752dd8e691e6fb3c8fba82e93d8a611",
+    "url": "/delundry/static/media/mobile.e752dd8e.svg"
+  },
+  {
+    "revision": "fb0574ef4edfe1134423f963d713102d",
+    "url": "/delundry/static/media/washer.fb0574ef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/delundry/precache-manifest.09d42d9993138c7e5d98c0f8074379aa.js"
+  "/delundry/precache-manifest.14935ba5f8ffbd335a415cfc7439711d.js"
 );
 
 self.addEventListener('message', (event) => {
