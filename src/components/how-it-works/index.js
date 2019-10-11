1d7c9e6b83f4560a826efbeea36b45645829554b
Yngrid Coello <yngrdyn@gmail.com>
2019-10-11 21:56:12 +0200

    adding steps

--- src/components/how-it-works/index.js
+++ src/components/how-it-works/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import mobile from "../../assets/mobile.svg";
+import washer from '../../assets/washer.svg';
+
 import './index.css';
 
 class HowItWorks extends Component {
@@ -12,13 +14,24 @@ class HowItWorks extends Component {
             <h3>Llevamos la lavandería hasta tu negocio o tu casa para que ahorres tiempo</h3>
           </div>
           <div className="steps">
-              <div className="step-img">
-                <img src={mobile} alt="Primer paso"></img>
-              </div>
-              <div className="step-info">
-                <span>1. Haces el pedido</span>
-                <p>Confirma la recogida y nosotros nos encargamos del resto</p>
-              </div>
+            <div className="step">
+                <div className="step-img">
+                  <img src={mobile} alt="Primer paso"></img>
+                </div>
+                <div className="step-info">
+                  <span>1. Haces el pedido</span>
+                  <p>Confirma la recogida y nosotros nos encargamos del resto</p>
+                </div>
+            </div>
+            <div className="step">
+                <div className="step-img">
+                  <img src={washer} alt="Segundo paso"></img>
+                </div>
+                <div className="step-info">
+                  <span>2. Nosotros lavamos</span>
+                  <p>Utilizando equipos de máxima calidad y personal especializado en lavandería textil</p>
+                </div>
+            </div>
           </div>
       </div>
     );
