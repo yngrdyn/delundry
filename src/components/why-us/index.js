7dea0acd9272746cd7a6fe73f4adb37528698e28
Yngrid Coello <yngrdyn@gmail.com>
2019-10-14 23:18:27 +0200

    why us explanantion

--- src/components/why-us/index.js
+++ src/components/why-us/index.js
@@ -6,8 +6,11 @@ import './index.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import medal from '../../assets/medal.svg';
 import bubbles from '../../assets/bubbles.svg';
+import euro from '../../assets/euro.svg';
+import medal from '../../assets/medal.svg';
+import professionalism from '../../assets/professionalism.svg';
+import sofa from '../../assets/sofa.svg';
 
 class WhyUs extends Component {
   render() {
@@ -20,6 +23,7 @@ class WhyUs extends Component {
       speed: 5000,
       autoplaySpeed: 1000,
       swipeToSlide: true,
+      swipe: true,
     };
 
     return (
@@ -29,19 +33,47 @@ class WhyUs extends Component {
             <div className="why">
               <img src={bubbles} alt="icono higiene"></img>
               <span className="title">Higiene</span>
+              <p className='content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam
+              </p>
             </div>
             <div className="why">
               <img src={medal} alt="icono alta calidad"></img>
               <span className="title">Alta calidad</span>
+              <p className='content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam
+              </p>
             </div>
             <div className="why">
+              <img src={euro} alt="icono precio insuperable"></img>
               <span className="title">Precio Insuperable</span>
+              <p className='content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam
+              </p>
             </div>
             <div className="why">
+              <img src={professionalism} alt="icono profesionalismo"></img>
               <span className="title">Profesionalismo</span>
+              <p className='content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam
+              </p>
             </div>
             <div className="why">
+              <img src={sofa} alt="icono coneveniencia"></img>
               <span className="title">Conveniencia</span>
+              <p className='content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam
+              </p>
             </div>
           </Slider>
       </div>
