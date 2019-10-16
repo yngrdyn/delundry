7bf6c623aeed7fca7293a48284eb4e82bc6decf2
Yngrid Coello <yngrdyn@gmail.com>
2019-10-16 21:56:45 +0200

    new logo

--- src/components/navbar/brand/index.js
+++ src/components/navbar/brand/index.js
@@ -3,14 +3,19 @@ import styled from "styled-components";
 
 import { Link } from 'react-router-dom';
 
-import machine from '../../../assets/washing-machine.svg';
+import './index.css';
+
+import IberiaWash from '../../../assets/IberiaWash.svg';
 
 const Brand = () => {
   return (
     <Link to="/">
       <Logo>
-        <span>Iberic Laundry</span>
-        <Image src={machine}></Image>
+        <Image src={IberiaWash}></Image>
+        <span className="text">
+          <span>Iberia Wash</span>
+          <span>Tu lavado textil online</span>
+        </span>
       </Logo>
     </Link>
   )
@@ -19,26 +24,28 @@ const Brand = () => {
 export default Brand
 
 const Image = styled.img`
-  width: 1rem;
+  width: 3rem;
+  margin-top: -1rem;
   @media (max-width: 768px) {
-    width: 1.1rem;
+    width: 2.2rem;
   }
 `;
 
 const Logo = styled.span`
-  width: 11rem;
+  width: 17rem;
   position: absolute;
   left: 2rem;
   top: 2rem;
   right: auto;
   color: white;
-  text-transform: uppercase;
   font-weight: bold;
   letter-spacing: 0.05rem;
   display: flex;
-  justify-content: space-evenly
+  font-family: 'Arista';
+  font-size: 1.8rem;
 
   @media (max-width: 768px) {
+    top: 2.5rem;
     right: 1rem;
     width: 11rem;
     left: auto;
